59cb39db045d8ee2c88aa5973149164d5d2bef2f
Viki Val <viktorko99@gmail.com>
2024-11-28 14:51:42 +0100

    :zap: questions easy + zustand

---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+export interface Question {
+  id: number;
+  question: string;
+  options: string[];
+  correctAnswer: number;
+}
+
+export interface QuizState {
+  currentQuestionIndex: number;
+  answers: number[];
+  score: number;
+  isComplete: boolean;
+}
