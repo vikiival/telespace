dc1637af2f1afde6f5ff5ae41ccef724ab94d99a
Viki Val <viktorko99@gmail.com>
2024-12-19 22:01:50 +0100

    :sparkles: next cointracker api

---
+++ src/api/cointracker/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// This is API to claim the reward
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const body = await req.json() as string[];
+    const claim = await fetch(`${process.env.API_URL}/api/claim`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body)
+    })
+    return NextResponse.json(claim);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(null, { status: 400 });
+  }
+}
